fdcbce822dbb6a6fe13d4ad10d7b4fb2f5083190
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-05-13 14:38:15 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
 
+int posizione(char nomi[10][20], char nome[20]){
+    for(int i=0;i<10; i++){
+        int j=0;
+        int count=0;
+        for (j=0;j<20;j++){
+            if(nomi[i][j]==nome[j]){
+                count++;
+            }
+        }if (count==20){
+            return i;
+        }
+    }
+    return -1;
+}
